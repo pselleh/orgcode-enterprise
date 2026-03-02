d78e123073890fef3801e0ae38527bb4c5388cdf
Phil Selleh <pselleh@gmail.com>
2026-03-02 02:03:49 +0000

    Fix ENV_PATCH to valid YAML for Ulmo registration

--- orgcode_enterprise/plugin.py
+++ orgcode_enterprise/plugin.py
@@ -1,10 +1,20 @@
 from tutor import hooks
 
+# Install package from GitHub
 hooks.Filters.CONFIG_DEFAULTS.add_item(
     (
         "OPENEDX_EXTRA_PIP_REQUIREMENTS",
         [
-            "-e ./plugins/orgcode-enterprise"
+            "git+https://github.com/pselleh/orgcode-enterprise.git@main"
         ],
     )
 )
+
+# Proper YAML patch for LMS (Ulmo-safe)
+hooks.Filters.ENV_PATCHES.add_item((
+    "lms-env",
+    """
+INSTALLED_APPS:
+  - orgcode_enterprise
+"""
+))
