2b6f4c9307227c2f076457fb0a4feb4e6e5c2a07
Phil Selleh <pselleh@gmail.com>
2026-03-02 01:27:13 +0000

    Stable installable version

--- orgcode_enterprise/plugin.py
+++ orgcode_enterprise/plugin.py
@@ -1,20 +1,10 @@
 from tutor import hooks
 
-# Mount local source directory into Docker build context
-hooks.Filters.IMAGES_BUILD_MOUNTS.add_item(
+hooks.Filters.CONFIG_DEFAULTS.add_item(
     (
-        "openedx",
-        "/home/cbaadmin/src/orgcode-enterprise",
-        "/mnt/orgcode-enterprise",
-    )
-)
-
-# Install the package into the openedx image
-hooks.Filters.IMAGES_BUILD.add_item(
-    (
-        "openedx",
-        """
-        RUN uv pip install -e /mnt/orgcode-enterprise
-        """,
+        "OPENEDX_EXTRA_PIP_REQUIREMENTS",
+        [
+            "-e ./plugins/orgcode-enterprise"
+        ],
     )
 )
