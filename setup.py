f5c5553a407356ee28414b0f8789ba94e841af08
Phil Selleh <pselleh@gmail.com>
2026-05-16 01:32:33 +0000

    Fix production packaging and Tutor image installation for orgcode_enterprise

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="tutor-orgcode-enterprise",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["tutor>=21.0.0"],
+    include_package_data=True,
     entry_points={
         "tutor.plugin.v1": [
             "orgcode_enterprise = orgcode_enterprise.plugin"
