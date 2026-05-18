c6b00b87d4bf564faf1b0365bec788ed55003921
Phil Selleh <pselleh@gmail.com>
2026-05-18 18:15:44 +0000

    Refactor for Ulmo Tutor 21 URL registration

--- orgcode_enterprise/apps.py
+++ orgcode_enterprise/apps.py
@@ -4,13 +4,3 @@ from django.apps import AppConfig
 class OrgcodeEnterpriseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orgcode_enterprise"
-
-    plugin_app = {
-        "url_config": {
-            "lms.djangoapp": {
-                "namespace": "orgcode_enterprise",
-                "regex": r"^api/orgcode/",
-                "relative_path": "urls",
-            }
-        }
-    }
--- orgcode_enterprise/plugin.py
+++ orgcode_enterprise/plugin.py
@@ -14,7 +14,7 @@ RUN pip install -e git+https://github.com/pselleh/orgcode-enterprise.git#egg=tut
 )
 
 # =========================================================
-# REGISTER DJANGO APP
+# REGISTER APP
 # =========================================================
 
 hooks.Filters.ENV_PATCHES.add_items(
@@ -22,16 +22,33 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "openedx-lms-production-settings",
             """
-if "orgcode_enterprise" not in INSTALLED_APPS:
-    INSTALLED_APPS.append("orgcode_enterprise")
+if "orgcode_enterprise.apps.OrgcodeEnterpriseConfig" not in INSTALLED_APPS:
+    INSTALLED_APPS.append("orgcode_enterprise.apps.OrgcodeEnterpriseConfig")
 """,
         ),
         (
             "openedx-lms-development-settings",
             """
-if "orgcode_enterprise" not in INSTALLED_APPS:
-    INSTALLED_APPS.append("orgcode_enterprise")
+if "orgcode_enterprise.apps.OrgcodeEnterpriseConfig" not in INSTALLED_APPS:
+    INSTALLED_APPS.append("orgcode_enterprise.apps.OrgcodeEnterpriseConfig")
 """,
         ),
     ]
+)
+
+# =========================================================
+# PATCH LMS URLCONF DIRECTLY
+# =========================================================
+
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "openedx-lms-urls",
+        """
+from django.urls import include, path
+
+urlpatterns.append(
+    path("api/orgcode/", include("orgcode_enterprise.urls"))
+)
+""",
+    )
 )
