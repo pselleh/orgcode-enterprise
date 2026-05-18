d302bf3ffe00f4c2670db652929ae1d3faef71df
Phil Selleh <pselleh@gmail.com>
2026-05-18 18:42:44 +0000

    Fix nested Django app registration

--- orgcode_enterprise/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrgcodeEnterpriseConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "orgcode_enterprise"
--- orgcode_enterprise/plugin.py
+++ orgcode_enterprise/plugin.py
@@ -14,7 +14,7 @@ RUN pip install -e git+https://github.com/pselleh/orgcode-enterprise.git#egg=tut
 )
 
 # =========================================================
-# REGISTER APP
+# REGISTER REAL DJANGO APP
 # =========================================================
 
 hooks.Filters.ENV_PATCHES.add_items(
@@ -22,33 +22,16 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "openedx-lms-production-settings",
             """
-if "orgcode_enterprise.apps.OrgcodeEnterpriseConfig" not in INSTALLED_APPS:
-    INSTALLED_APPS.append("orgcode_enterprise.apps.OrgcodeEnterpriseConfig")
+if "orgcode_enterprise.apps.orgcode" not in INSTALLED_APPS:
+    INSTALLED_APPS.append("orgcode_enterprise.apps.orgcode")
 """,
         ),
         (
             "openedx-lms-development-settings",
             """
-if "orgcode_enterprise.apps.OrgcodeEnterpriseConfig" not in INSTALLED_APPS:
-    INSTALLED_APPS.append("orgcode_enterprise.apps.OrgcodeEnterpriseConfig")
+if "orgcode_enterprise.apps.orgcode" not in INSTALLED_APPS:
+    INSTALLED_APPS.append("orgcode_enterprise.apps.orgcode")
 """,
         ),
     ]
-)
-
-# =========================================================
-# PATCH LMS URLCONF DIRECTLY
-# =========================================================
-
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-lms-urls",
-        """
-from django.urls import include, path
-
-urlpatterns.append(
-    path("api/orgcode/", include("orgcode_enterprise.urls"))
-)
-""",
-    )
 )
